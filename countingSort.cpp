56d1a71755f72ff88fc8964b2d40f512bfcfb090
Mug1vara97 <mugivarakun97@gmail.com>
2024-01-18 22:10:40 +0300

    4

--- countingSort.cpp
+++ countingSort.cpp
@@ -4,19 +4,20 @@
 using namespace std;
 
 void countingSort(vector<int>& Array) {
+//ищем максимальное значение в массиве
     int maxValue = Array[0];
     for (int i = 1; i< Array.size(); i++) {
         if (Array[i] > maxValue) {
             maxValue = Array[i];
         }
     }
-
+//создание массива для подсчёта и инициализации его нулями
     vector<int> count(maxValue + 1, 0);
-
+//счёт колисества вхождений каждого элеиента в исходном массиве
     for ( int i = 0; i < Array.size(); i++) {
         count[Array[i]]++;
     }
-
+//Записб отсортированных элементов в новый массив
     int Index = 0;
     for (int i = 0; i < count.size(); i++) {
         for (int j = 0; j < count[i]; j++) {
